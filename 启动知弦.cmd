569f3811e4dcbb6dc3bbdb28eba0e9d87e2465a9
DiscoveryH2 <293590121+DiscoveryH2@users.noreply.github.com>
2026-09-23 22:05:46 +0800

    Prefer the new local Windows build in launcher

--- 启动知弦.cmd
+++ 启动知弦.cmd
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
-if exist "outputs\build\Zhixian\Zhixian.exe" (
+if exist "outputs\build-v1.2\Zhixian\Zhixian.exe" (
+  start "" "outputs\build-v1.2\Zhixian\Zhixian.exe" --data-dir "%~dp0data"
+) else if exist "outputs\build\Zhixian\Zhixian.exe" (
   start "" "outputs\build\Zhixian\Zhixian.exe"
 ) else if exist "outputs\Zhixian\Zhixian.exe" (
   start "" "outputs\Zhixian\Zhixian.exe"
