e5dd8c142b868ef97332a5818c82eada903e551c
DiscoveryH2 <293590121+DiscoveryH2@users.noreply.github.com>
2026-09-24 22:38:05 +0800

    Launch the local 1.5.0 build by default

--- 启动知弦.cmd
+++ 启动知弦.cmd
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
-if exist "outputs\build-v1.4.4\Zhixian\Zhixian.exe" (
+if exist "outputs\build-v1.5.0\Zhixian\Zhixian.exe" (
+  start "" "outputs\build-v1.5.0\Zhixian\Zhixian.exe" --data-dir "%~dp0data"
+) else if exist "outputs\build-v1.4.4\Zhixian\Zhixian.exe" (
   start "" "outputs\build-v1.4.4\Zhixian\Zhixian.exe" --data-dir "%~dp0data"
 ) else if exist "outputs\build-v1.4.3\Zhixian\Zhixian.exe" (
   start "" "outputs\build-v1.4.3\Zhixian\Zhixian.exe" --data-dir "%~dp0data"
